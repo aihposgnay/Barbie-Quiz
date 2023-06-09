b48d34d5e60db0c2c1f5ce139fc4c5a60c423a01
Sophia Yang <aihposgnay@gmail.com>
2023-06-09 10:27:48 -0500

    Initial Commit

---
+++ Barbie Quiz/Barbie_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barbie_QuizApp.swift
+//  Barbie Quiz
+//
+//  Created by scholar on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Barbie_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
